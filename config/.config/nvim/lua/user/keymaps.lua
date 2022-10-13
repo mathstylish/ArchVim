c5b9cc5d2e042d309c537c3997f9e5018ab2280d
Matheus Henrique <matheus.hpp135@gmail.com>
2022-10-13 17:37:39 -0300

    Basic keymaps

---
+++ config/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,47 @@
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- LEADER
+vim.g.mapleader = " "
+
+-- INSERT
+-- exit insert
+keymap("i", "kj", "<Esc>", opts)
+
+-- NORMAL
+-- quit (this exit vim if have one buffer)
+keymap("n", "<leader>q", ":quit<CR>", opts)
+
+-- save
+keymap("n", "<leader>w", ":write<CR>", opts)
+
+-- better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- split editor
+keymap("n", "<leader>vs", ":vsplit<CR>", opts)
+keymap("n", "<leader>hs", ":split<CR>", opts)
+
+-- clear highlights
+keymap("n", "<leader>h", ":nohlsearch<CR>", opts)
+
+-- delete a word backwards
+keymap("n", "db", 'vb"_d')
+
+-- VISUAL
+-- stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- TWO OR MORE MODES
+-- '-' to go end of line
+keymap({"n", "v"}, "-", "$", opts)
